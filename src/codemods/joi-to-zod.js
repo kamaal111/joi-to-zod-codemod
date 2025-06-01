9e3d079cb3a0be7f8ca5d4cadcf54fa6cb23392a
Kamaal Farah <kamaal.f1@gmail.com>
2025-06-01 19:59:54 +0000

    Collecting enums for joi schema

--- src/codemods/joi-to-zod.js
+++ src/codemods/joi-to-zod.js
@@ -11,27 +11,40 @@ import { jsonSchemaToZod } from 'json-schema-to-zod';
  */
 function transform(fileInfo, api) {
   const j = api.jscodeshift;
+  const source = j(fileInfo.source);
+  /**
+   * @type {Record<string, import('jscodeshift').ASTPath<import('jscodeshift').EnumDeclaration>}
+   */
+  const globalEnumsMappedByIdentifierName = findAllGlobalEnums(j, source)
+    .paths()
+    .reduce((acc, p) => {
+      const id = p.value.id;
+      j.Identifier.assert(id);
+      acc[id.name] = p;
 
-  return j(fileInfo.source)
+      return acc;
+    }, {});
+  source
     .find(j.VariableDeclarator, { init: i => i != null })
-    .forEach(p => {
+    .paths()
+    .map(p => {
       const { init } = p.value;
-      if (init == null) return;
+      if (init == null) return null;
 
       const initSource = j(init).toSource();
-      if (!initSource.toLowerCase().includes('joi')) return;
+      if (!initSource.toLowerCase().includes('joi')) return null;
 
-      // Actually collect all that is necessary for the Joi object
-      const initSourceWithExtraThings = `
-        enum Job {
-          Developer = 'developer',
-          DevOps = 'devops',
-          Designer = 'designer',
-        }
+      const joiSource = j(init)
+        .find(j.Identifier, { name: n => globalEnumsMappedByIdentifierName[n] != null })
+        .nodes()
+        .map(n => j(globalEnumsMappedByIdentifierName[n.name].value).toSource())
+        .concat(initSource)
+        .join('\n\n');
 
-        ${initSource}
-      `;
-
+      return { path: p, source: joiSource.trim() };
+    })
+    .filter(v => v != null)
+    .forEach(({ source }) => {
       try {
         global.Joi = Joi;
         global.joi = Joi;
@@ -39,13 +52,23 @@ function transform(fileInfo, api) {
         global.J = Joi;
         global.j = Joi;
 
-        const zodSchema = jsonSchemaToZod(joiToJson(global.eval(ts.transpile(initSourceWithExtraThings))));
+        const zodSchema = jsonSchemaToZod(joiToJson(global.eval(ts.transpile(source))));
         console.log('🐸🐸🐸', zodSchema);
       } catch {
         return;
       }
-    })
-    .toSource();
+    });
+
+  return source.toSource();
+}
+
+/**
+ * @param {import('jscodeshift').JSCodeshift} j
+ * @param {import('jscodeshift').Collection} source
+ * @returns {import('jscodeshift').Collection<import('jscodeshift').EnumDeclaration>}
+ */
+function findAllGlobalEnums(j, source) {
+  return source.find(j.TSEnumDeclaration).filter(p => p.scope.isGlobal);
 }
 
 export default transform;
