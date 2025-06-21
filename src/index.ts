98fc14fdfd45347b6c98242ec2e8fab0da7d0f48
Kamaal Farah <kamaal.f1@gmail.com>
2025-06-21 22:16:51 +0200

    Exporting joi to zod transformer language

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { run } from '@oclif/core';
-export { default as joiToZodTransformer, joiToZod } from './codemods/joi-to-zod/index.js';
+export { default as joiToZodTransformer, joiToZod, JOI_TO_ZOD_LANGUAGE } from './codemods/joi-to-zod/index.js';
 export type { Modifications } from './codemods/types.js';
