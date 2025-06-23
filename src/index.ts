dfa7c3bfaf894898a53d8aa97f5c64b6886efa99
Kamaal Farah <kamaal.f1@gmail.com>
2025-06-23 08:18:30 +0200

    Exporting transformer as default

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
 export { run } from '@oclif/core';
-export {
-  default as joiToZodTransformer,
-  joiToZod,
-  JOI_TO_ZOD_LANGUAGE,
-  JOI_TO_ZOD_CODEMOD,
-} from './codemods/joi-to-zod/index.js';
+export { default, joiToZodTransformer, JOI_TO_ZOD_LANGUAGE, JOI_TO_ZOD_CODEMOD } from './codemods/joi-to-zod/index.js';
